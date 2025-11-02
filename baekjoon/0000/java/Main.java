fa44e4f6279698ffae81fd812b073e3790b03473
chang2kim <111842836+chang2kim@users.noreply.github.com>
2025-11-02 23:21:46 +0900

    [chore] 템플릿 수정

--- baekjoon/0000/java/Main.java
+++ baekjoon/0000/java/Main.java
@@ -4,10 +4,16 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
+		// 입력 1행
+		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		System.out.println(N);
+		// 출력
+		sb.append(N).append(" ").append(M);
+		System.out.println(sb.toString().trim());
 	}
 }
